281b5b4ae3a91cfca6e52f557ea4e348b6656237
Do Tran <vivaladaman@gmail.com>
2024-12-04 12:52:20 +0700

    .

---
+++ src/summarize.js
@@ -0,0 +1,68 @@
+export const Summarize = {
+name: "Tran Van Do",
+	position: ".NET Developer",
+	contact: {
+		address: "Go Vap District, Ho Chi Minh City",
+		phone: "+84 774 276 286",	
+		email: "tvddaman@gmail.com",
+		linkedin: "/tvddaman",
+	},
+	education: [
+		{
+			school: "Cao Thang College",
+			major: "IT"
+		}	
+	],
+	certificates: [
+		{
+			name: "AZ 900",
+			link: "https://google.com/"
+		}
+	],
+	skills: [
+		".NET Core",
+		"C#",
+		"EF Core",
+		"Javascript",
+		"HTMl 5",
+		"CSS 3",
+		"Angular",
+		"React",
+		"Rust",
+		"Git",
+		"Azure Cloud",
+		"Docker"
+	],
+	workHistories: [
+		{
+			company: "Sai Gon Technology",
+			link: "",
+			position: "Senior Developer",
+			from: "May - 2021",
+			to: "Now",
+			project: [
+				{
+					name: "Idealpos",
+					description: "Cloud-POS System"	,
+					headCount: 6,
+					position: ".NET developer",
+					mainResponsibility: "Develop new features, maintain old features",
+					technology: ".NET Framework, .NET Core, EF Framework, EF Core, Azure SQL",
+					additionalInfo: {},
+				}
+			]
+		}
+	],
+	projects: [
+		{
+			name: "Shibuicat Universe",
+			link : "",
+			description: "It's a framework for self development"	,
+			headCount: 1,
+			position: "Solo developer",
+			mainResponsibility: "Develop/ maintain all the features",
+			technology: ".NET Core, EF Core, AstroJS, PostgreSQL, Rust, Self Hosting, AWS",
+			additionalInfo: {},
+		}
+	]
+};
